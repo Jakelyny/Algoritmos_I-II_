b1d2a316d56603d053064229335f08623fb52851
Jakelyny Monroe <jakelynysousa.a@gmail.com>
2021-06-02 14:12:51 -0300

    .

---
+++ Algoritmos I/38a_condicional.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+/*Um banco concederá um crédito especial aos seus clientes, variável com o saldo médio no
+último ano. Faça um algoritmo que leia o saldo médio de um cliente e calcule o valor do
+crédito de acordo com a tabela abaixo.Mostre uma mensagem informando o saldo médio e o valor do crédito*/
+
+int main()
+{
+    double saldoMedio, credito, valPorc;
+
+    cout<<"Informe o saldo medio do cliente: ";
+    cin>>saldoMedio;
+
+    if (saldoMedio>=0 && saldoMedio<=200)
+    {
+        cout<<"Informe a porcentagem entre 0 e 200: ";
+        cin>>valPorc;
+        credito=saldoMedio*valPorc;
+    }
+    else if (saldoMedio>=201 && saldoMedio<=400)
+        credito=saldoMedio*0.2;
+    else if (saldoMedio>=401 && saldoMedio<=600)
+        credito=saldoMedio*0.3;
+    else
+        credito=saldoMedio*0.4;
+
+    cout<<"\n O saldo medio e: "<<saldoMedio;
+    cout<<"\n O credito e: "<<credito;
+
+    cout<<"\n \n";
+}
