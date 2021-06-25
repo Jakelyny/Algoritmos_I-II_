0f4e34d330f637eeded668947d45dddc90fb01dc
Jakelyny Monroe <jakelynysousa.a@gmail.com>
2021-06-25 08:46:20 -0300

    .

---
+++ Algoritmos II/Prova 1/01.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void Verifica(char numero[]);
+void Permuta(char numero[]);
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+    char numero[5];
+    int tamanho = strlen(numero);
+
+    cout<<" Digite um número para saber sua permutação: ";
+    gets(numero);
+    Verifica(numero);
+    getchar();
+    return 0;
+}
+
+void Verifica(char numero[]){
+    if(atoi(numero) < 9999){
+        cout<<endl;
+        Permuta(numero);
+    }else{
+        cout<<"\n\n\t O NÚMERO PRECISA SER MENOR QUE 9999!\n\n";
+    }
+}
+
+void Permuta(char numero[]){
+    cout<<"--PERMUTAÇÃO--"<<endl;
+    switch(strlen(numero)){
+    case 1:
+        cout<<numero[0];
+        break;
+    case 2:
+        do {
+            cout<<"  "<<numero[0]<<" "<<numero[1]<<endl;
+        } while(next_permutation(numero, numero + 2));
+        break;
+    case 3:
+        do {
+            cout<<"  "<<numero[0]<<" "<<numero[1]<<" "<<numero[2]<<endl;
+        } while(next_permutation(numero, numero + 3));
+        break;
+    case 4:
+        do {
+            cout<<"  "<<numero[0]<<" "<<numero[1]<<" "<<numero[2]<<" "<<numero[3]<<endl;
+        } while(next_permutation(numero, numero + 4));
+        break;
+    default:
+        break;
+    }
+    cout<<"--------------"<<endl;
+}
