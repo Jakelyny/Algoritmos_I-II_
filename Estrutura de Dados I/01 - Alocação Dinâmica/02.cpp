eacf209051b13e40ae95a16580851419876494c1
Jakelyny Monroe <jakelynysousa.a@gmail.com>
2021-07-13 08:41:46 -0300

    Update 02.cpp

--- Estrutura de Dados I/01 - Alocação Dinâmica/02.cpp
+++ Estrutura de Dados I/01 - Alocação Dinâmica/02.cpp
@@ -2,6 +2,11 @@
 #include <ctime>
 using namespace std;
 
+/*Escreva um programa que solicita ao usuário a quantidade de alunos de uma turma e aloca um vetor de notas
+(números reais). Depois de ler as notas, imprime a média aritmética. Obs: não deve ocorrer desperdício de
+memória; e após ser utilizada a memória deve ser devolvida.*/
+
+
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
