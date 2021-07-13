9d0e1a15ac64117290061939667e91091b3c0f01
Jakelyny Monroe <jakelynysousa.a@gmail.com>
2021-07-13 08:43:17 -0300

    Update 02.cpp

--- Estrutura de Dados I/01 - Alocação Dinâmica/02.cpp
+++ Estrutura de Dados I/01 - Alocação Dinâmica/02.cpp
@@ -6,7 +6,6 @@ using namespace std;
 (números reais). Depois de ler as notas, imprime a média aritmética. Obs: não deve ocorrer desperdício de
 memória; e após ser utilizada a memória deve ser devolvida.*/
 
-
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
