a9a55838f3fc6473ba9623fb85e559b105358adb
Jakelyny Monroe <jakelynysousa.a@gmail.com>
2021-07-13 08:45:08 -0300

    Update 03.cpp

--- Estrutura de Dados I/01 - Alocação Dinâmica/03.cpp
+++ Estrutura de Dados I/01 - Alocação Dinâmica/03.cpp
@@ -2,7 +2,9 @@
 #include <ctime>
 using namespace std;
 
-/* */
+/* Faça um programa que crie dinamicamente um vetor de n elementos e passe esse vetor para uma função que
+vai ler os elementos desse vetor. Depois, no programa principal (main), o vetor preenchido deve ser impresso.
+Além disso, antes de finalizar o programa, deve-se liberar a área de memória alocada.*/
 
 void criaVetor(int quant, int *vet);
 
